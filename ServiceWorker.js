9225aa41608857975da022dcd111bffed2a4df9c
Cristian Perez <crisselmai10@hotmail.es>
2025-05-20 16:01:38 -0500

    Clean chace

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,33 +1,6 @@
-const cacheName = "CBI-WebtoolServersEnables-0.1.0";
-const contentToCache = [
-    "Build/Build_Webtool.loader.js",
-    "Build/Build_Webtool.framework.js",
-    "Build/Build_Webtool.data",
-    "Build/Build_Webtool.wasm",
-    "TemplateData/style.css"
-
-];
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
     
-    e.waitUntil((async function () {
-      const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
-      await cache.addAll(contentToCache);
-    })());
 });
 
-self.addEventListener('fetch', function (e) {
-    e.respondWith((async function () {
-      let response = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (response) { return response; }
-
-      response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
-    })());
-});
